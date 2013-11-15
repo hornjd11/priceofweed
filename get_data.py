483c95e94f1020746c58e746cd4e05eb7844773a
Zachary M. Jones <zmj@zmjones.com>
2013-11-15 18:13:24 -0500

    PEP8 compliance

--- get_data.py
+++ get_data.py
@@ -4,12 +4,13 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 
+
 def parse_table(soup, df):
     table = soup.find("table")
     for row in table.find_all("tr")[1:]:
         data = list(row.find_all("td"))
         for i in range(len(data)):
-           data[i] = data[i].get_text(strip = True)
+            data[i] = data[i].get_text(strip=True)
         df.append(data)
     return df
 
@@ -19,9 +20,10 @@ while True:
     soup = BeautifulSoup(requests.get(url).content)
     df = parse_table(soup, df)
     try:
-        url = soup.find("div", id = "pagination").find("a", text = "Next")["href"]
+        url = soup.find("div", id="pagination").find("a", text="Next")["href"]
     except:
         break
 
-df = pd.DataFrame(df, columns = ["location", "price", "amount", "quality", "date"])
-df.to_csv("./data/mj_raw.csv", na_rep = "NA", index = False, encoding = "utf-8")
+df = pd.DataFrame(df, columns=["location", "price",
+                               "amount", "quality", "date"])
+df.to_csv("./data/mj_raw.csv", na_rep="NA", index=False, encoding="utf-8")
